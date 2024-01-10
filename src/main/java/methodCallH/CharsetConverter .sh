abd552ecb16853f22f51678758238e080d9a1918
61424Crab <138338550+61424Crab@users.noreply.github.com>
2024-01-10 09:50:55 +0900

    Update CharsetConverter .sh

--- src/main/java/methodCallH/CharsetConverter .sh
+++ src/main/java/methodCallH/CharsetConverter .sh
@@ -7,14 +7,13 @@ find "$DIRECTORY" -type f -iname "*.sql" | while read -r file; do
     # 检测文件编码
     charset=$(file -bi "$file" | sed -e 's/.*[ ]charset=//')
 
-    # 如果编码不是 utf-8 或 us-ascii（us-ascii 可以不变），则转换
-    if [ "$charset" != "utf-8" ] && [ "$charset" != "us-ascii" ]; then
-        echo "Converting $file from $charset to utf-8"
-        iconv -f "$charset" -t "UTF-8" "$file" -o "${file}.tmp" && mv "${file}.tmp" "$file"
-        
-        if [ $? -ne 0 ]; then
-            echo "Error converting file: $file"
-            rm "${file}.tmp"
-        fi
+if [ "$charset" = "shift-jis" ]; then
+    echo "Converting $file from Shift-JIS to UTF-8"
+    iconv -f SHIFT-JIS -t UTF-8 "$file" -o "${file}.tmp" && mv "${file}.tmp" "$file"
+    
+    if [ $? -ne 0 ]; then
+        echo "Error converting file: $file"
+        rm "${file}.tmp"
     fi
+fi
 done
